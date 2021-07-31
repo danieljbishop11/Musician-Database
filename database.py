eb8788aef927e6564b933a19da13e51ec2720f25
Daniel Lepiscopo <danieljlepiscopo@gmail.com>
2021-07-31 09:16:40 -0600

    Created table, colums, and basic data.

--- database.py
+++ database.py
@@ -1,1 +1,18 @@
-print('Hello Database!')
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    passwd="123password",
+    database="musiciandb"
+)
+
+mycursor = mydb.cursor()
+
+sql = "ALTER TABLE producers CHANGE 'name' TO 'First Name'"
+
+mycursor.execute(sql)
+
+mydb.commit()
+
+mydb.close()
